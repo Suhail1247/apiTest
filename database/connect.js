25c09e107ff4bbb852b5acc318f0c8b05915eb97
ss12 <suhaila@my-mac.local>
2024-07-17 15:00:25 +0530

    update

--- database/connect.js
+++ database/connect.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 async function connect() {
-    const db = await mongoose.connect(process.env.MONGO);
+    const db = await mongoose.connect('mongodb+srv://suhailwt12:hsUJqXxEanctYkO0@cluster0.1tcxazi.mongodb.net/?retryWrites=true&w=majority');
     console.log('Connected to MongoDB ');
     return db;
   }
