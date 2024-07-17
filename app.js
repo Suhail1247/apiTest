4ca250a3d498678035e8961de561e8e645df3895
ss12 <suhaila@my-mac.local>
2024-07-17 14:57:41 +0530

    update

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
 import express from 'express';
 import connect from './database/connect.js'
+import cors from 'cors';
 import router from './router/route.js';
-import bodyParser from "body-parser"
+import dotenv from 'dotenv';
+
+dotenv.config();
 const app=express()
 
-
+app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(bodyParser.json());
 
-
-
-const port=8080
 
 
 app.use('/api',router)
 
 connect().then(()=>{
     try{
-        app.listen(port,()=>{
+        app.listen(process.env.port,()=>{
             console.log('connected to the port');
         })
     }catch{
         console.log('cannot connect to the server')
     }
 }).catch(err=>{
-    console.log('invalid connection',err)
+    console.log('invalid connection')
 })
-
